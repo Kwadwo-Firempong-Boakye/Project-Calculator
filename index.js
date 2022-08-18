df26bc0653d40ade1c4c3557f57afa813fc4478e
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-18 20:39:21 +0000

    write operate function to choose correct operation

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const multiply = function (firstInput, secondInput) {
 };
 
 const divide = function (firstInput, secondInput) {
-	if ((secondInput = 0)) {
+	if (secondInput === 0) {
 		return "indivisible";
+	} else {
+		return firstInput / secondInput;
 	}
-	return firstInput / secondInput;
 };
 
 const exponent = function (firstInput, secondInput) {
@@ -26,3 +27,47 @@ const exponent = function (firstInput, secondInput) {
 const root = function (firstInput) {
 	return Math.sqrt(firstInput);
 };
+
+// Calculator Operate Function
+
+const operate = function (firstInput, operator, secondInput) {
+	let output;
+
+	switch (operator) {
+		case "+":
+			output = add(firstInput, secondInput);
+			break;
+		case "-":
+			output = subtract(firstInput, secondInput);
+			break;
+		case "*":
+			output = multiply(firstInput, secondInput);
+			break;
+		case "/":
+			output = divide(firstInput, secondInput);
+			break;
+		case "exp":
+			output = exponent(firstInput, secondInput);
+			break;
+		case "root":
+			output = root(firstInput);
+			break;
+		default:
+			output = 0;
+			break;
+	}
+
+	//Limit Output Length
+
+	if (
+		(output < 1 && output.toString().length > 9) ||
+		output.toString().length > 9
+	) {
+		return output.toExponential(9);
+	} else {
+		return output;
+	}
+};
+
+// let test = operate(10000000000, "+", 1);
+// console.log(test);
