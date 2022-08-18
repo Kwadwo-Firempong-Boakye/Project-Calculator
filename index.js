f4500c9c4258ee8b1951d9f09fa2af7d953eb2cb
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-18 19:39:38 +0000

    write functions to add, subtract, multiply, divide

---
+++ index.js
@@ -0,0 +1,28 @@
+//Calculator Operator Functions
+
+const add = function (firstInput, secondInput) {
+	return firstInput + secondInput;
+};
+
+const subtract = function (firstInput, secondInput) {
+	return firstInput - secondInput;
+};
+
+const multiply = function (firstInput, secondInput) {
+	return firstInput * secondInput;
+};
+
+const divide = function (firstInput, secondInput) {
+	if ((secondInput = 0)) {
+		return "indivisible";
+	}
+	return firstInput / secondInput;
+};
+
+const exponent = function (firstInput, secondInput) {
+	return Math.pow(firstInput, secondInput);
+};
+
+const root = function (firstInput) {
+	return Math.sqrt(firstInput);
+};
