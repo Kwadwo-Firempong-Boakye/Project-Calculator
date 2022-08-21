62312cc90d8bec6af161eaf21a0147c1e7d80c82
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-21 01:08:19 +0000

    add working operate functions for operator buttons

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const operate = function (firstInput, operator, secondInput) {
 		(output < 1 && output.toString().length > 9) ||
 		output.toString().length > 9
 	) {
-		return output.toExponential(9);
+		return output.toExponential(4);
 	} else {
 		return output;
 	}
@@ -73,11 +73,18 @@ const operate = function (firstInput, operator, secondInput) {
 
 //Global Variables
 let input = "";
+let firstInputValue = 0;
+let secondInputValue = 0;
+let operatorValue = "";
+let currentDisplayValue = 0;
 
 //DOM Definitions
 
 let displayText = document.querySelector(".calc-text");
+let displaySubtext = document.querySelector(".calc-subtext");
 const calcButtons = document.querySelectorAll(".button");
+let operatorButtons = document.querySelectorAll(".operator");
+const enterButton = document.querySelector(".enter");
 
 //DOM Event Listeners
 window.addEventListener("keydown", keyboardSupport);
@@ -85,6 +92,13 @@ window.addEventListener("keydown", keyboardSupport);
 calcButtons.forEach((button) => {
 	button.addEventListener("click", clickSupport);
 });
+
+operatorButtons.forEach((button) => {
+	button.addEventListener("keydown", mountInitialValues);
+	button.addEventListener("click", mountInitialValues);
+});
+
+enterButton.addEventListener("click", startOperation);
 
 //DOM Functions
 
@@ -111,7 +125,27 @@ function clickSupport(e) {
 	displayText.innerText = input;
 }
 
+// Calculator Procedure Functions
 
-function stringToNumber (string) {
-	return +(string);
+function mountInitialValues (e) {
+	operatorValue = e.target.getAttribute("data-key");
+
+	if (currentDisplayValue != 0) {
+		firstInputValue = +(currentDisplayValue);
+		displaySubtext.innerText = `${currentDisplayValue} ${operatorValue}`;
+	} else {
+		firstInputValue = +(input);
+		displaySubtext.innerText = `${firstInputValue} ${operatorValue}`;
+	}
+
+	input = "";
+	displayText.innerText = input;
 }
+
+function startOperation () {
+	secondInputValue = +(input);
+	displaySubtext.innerText += ` ${secondInputValue}`;
+	currentDisplayValue = operate(firstInputValue, operatorValue, secondInputValue);
+	displayText.innerText = currentDisplayValue;
+}
+
