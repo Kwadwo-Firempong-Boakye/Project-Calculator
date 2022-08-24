ad597d8ea2df578abe6fd4d8e430b2f7dc9cc40f
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-24 01:03:55 +0000

    add 1/x operation support with turnFraction()

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let tempOperatorValue;
 let currentOperationValue = 0;
 let operationsCount = 0;
 let endOperationCount = 0;
-let operateButtonPressCount = 0
+let operateButtonPressCount = 0;
 let isZeroInput = "no";
 
 //Calculator Operator Functions
@@ -129,7 +129,6 @@ function clickSupport(e) {
 	if (isBackspace == "Backspace") {
 		let updatedInput = input.slice(0, input.length - 1);
 		input = updatedInput;
-
 	} else if (isDisplayEligible == "yes" && input.length < 9) {
 		if (endOperationCount != 0 && operateButtonPressCount == 0) {
 			resetGlobal();
@@ -142,7 +141,6 @@ function clickSupport(e) {
 		} else {
 			decimal.setAttribute("data-display", "yes");
 		}
-			
 	}
 	displayText.innerText = input;
 }
@@ -151,7 +149,6 @@ function keyboardSupport(e) {
 	const keyButton = document.querySelector(`.button[data-key="${e.key}"]`);
 
 	if (keyButton !== null) {
-
 		let isBackspace = keyButton.getAttribute("data-key");
 		let isDisplayEligible = keyButton.getAttribute("data-display");
 		let isOperator = keyButton.getAttribute("class");
@@ -161,7 +158,6 @@ function keyboardSupport(e) {
 			let updatedInput = input.slice(0, input.length - 1);
 			input = updatedInput;
 			displayText.innerText = input;
-
 		} else if (isDisplayEligible == "yes" && input.length < 9) {
 			if (endOperationCount != 0 && operateButtonPressCount == 0) {
 				resetGlobal();
@@ -175,15 +171,12 @@ function keyboardSupport(e) {
 			}
 
 			displayText.innerText = input;
-
 		} else if (isOperator == "operator button") {
 			operatorValue = keyButton.getAttribute("data-key");
 			startOperations();
 			// displayText.innerText = input;
-
 		} else if (isEnter == "Enter") {
 			endOperation();
-
 		}
 	}
 }
@@ -191,31 +184,40 @@ function keyboardSupport(e) {
 // Calculator Procedure Functions
 
 function startOperations() {
-
 	operateButtonPressCount++;
 
-	if (operationsCount == 0) { 
-
+	if (operationsCount == 0) {
 		if (+input == 0) {
 			displaySubtext.innerText = `${currentOperationValue} ${operatorValue}`;
 			input = "";
 			displayText.innerText = input;
 			tempOperatorValue = operatorValue;
 			isZeroInput = "yes";
-
 		} else {
 			if (tempOperatorValue === undefined) {
 				tempOperatorValue = operatorValue;
 			}
 
-			if (currentOperationValue == 0 && (operatorValue == "*" || operatorValue == "/") && isZeroInput == "no") {
+			if (
+				currentOperationValue == 0 &&
+				(operatorValue == "*" || operatorValue == "/") &&
+				isZeroInput == "no"
+			) {
 				currentOperationValue = 1;
-			} else if (currentOperationValue == 0 && (operatorValue == "*" || operatorValue == "/") && isZeroInput == "yes") {
+			} else if (
+				currentOperationValue == 0 &&
+				(operatorValue == "*" || operatorValue == "/") &&
+				isZeroInput == "yes"
+			) {
 				currentOperationValue = 0;
 			}
 
 			firstInputValue = +input;
-			currentOperationValue = operate(firstInputValue, tempOperatorValue, currentOperationValue);
+			currentOperationValue = operate(
+				firstInputValue,
+				tempOperatorValue,
+				currentOperationValue
+			);
 
 			if (currentOperationValue.length > 9) {
 				let fixedLength = (+currentOperationValue).toPrecision(5);
@@ -227,18 +229,14 @@ function startOperations() {
 			displayText.innerText = input;
 			tempOperatorValue = operatorValue;
 			isZeroInput = "no";
-
 		}
-
 	} else if (operationsCount > 0) {
-
 		if (input == "") {
 			//Do not operate. Just change operation variables
 			displaySubtext.innerText = `${currentOperationValue} ${operatorValue}`;
 			input = "";
 			displayText.innerText = input;
 			tempOperatorValue = operatorValue;
-
 		} else {
 			secondInputValue = +input;
 			currentOperationValue = operate(
@@ -256,19 +254,17 @@ function startOperations() {
 			tempOperatorValue = operatorValue;
 			input = "";
 			displayText.innerText = input;
-
 		}
 	}
 }
 
-function endOperation () {
+function endOperation() {
 	//display currentvalue in main display and subdisplay, set input to empty, set operatorvalue to empty
 	startOperations();
 	displayText.innerText = currentOperationValue;
 	displaySubtext.innerText = currentOperationValue;
-	endOperationCount ++;
+	endOperationCount++;
 	operateButtonPressCount = 0;
-	
 }
 
 function resetGlobal() {
@@ -291,12 +287,49 @@ function negate() {
 		input = neg.concat(input);
 	} else if (Math.sign(input) < 0) {
 		input = input.slice(1);
-	} else if (input ="") {
-		return
+	} else if ((input = "")) {
+		return;
 	}
 	displayText.innerText = input;
 }
 
-function turnFraction () {
+function turnFraction() {
+	if (input != "") {
+		input = (1 / +input);
+		
+		if (input == Infinity || input == -Infinity){
+			input = 0;
+		}
+		
+		input = input.toString()
 
+		if (input.length > 8) {
+			alert(`Sorry, this calculator can only process figures with less than 10 digits. \n\nYour calculated number: ${input} will be supported in an update soon. \n\nThank you for testing my build.`)
+			resetGlobal();
+		}
+
+		displayText.innerText = input;
+
+
+	} else {
+		currentOperationValue = 1 / +currentOperationValue;
+		let strVersion = currentOperationValue.toString();
+		if (strVersion.length > 8) {
+			alert(`Sorry, this calculator can only process figures with less than 10 digits. \n\nYour calculated number: ${currentOperationValue} will be supported in an update soon. \n\nThank you for testing my build.`)
+			resetGlobal();
+		}
+
+		if (currentOperationValue == Infinity || currentOperationValue == -Infinity){
+			currentOperationValue = 0;
+		}
+
+		displayText.innerText = currentOperationValue;
+
+	}
+
+	endOperationCount++;
+	operateButtonPressCount = 0;
+	
 }
+
+///SOLVE TURN FRACTION USE ON OUTPUT AND IN MOTION
